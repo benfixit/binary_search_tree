df214c400a28eeffa51926955948d5c3b62b6777
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-08-10 12:41:59 +0100

    Implement PreOrder Iterative

---
+++ exercises/pre_order_iterative.js
@@ -0,0 +1,23 @@
+const { BSTObject } = require('../index')
+
+function preOrderIterative (node) {
+    if (node === null){
+        return;
+    }
+    const stack = [];
+    stack.push(node);
+    while(stack.length > 0){
+        let currentNode = stack.pop()
+        console.log(currentNode.data)
+        
+        if (currentNode.right){
+            stack.push(currentNode.right)
+        } 
+        if (currentNode.left){
+            stack.push(currentNode.left)
+        }
+    }
+    
+}
+
+preOrderIterative(BSTObject.root)
